32e9d08ba8f1232f214ccd019a386bf7f46da2f5
Michael Witlox <michael.witlox@greenhousegroup.com>
2020-12-08 11:29:05 +0100

    Improved performance by using combinations

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -10,31 +10,26 @@
 
 (def assignment-data (map #(read-string %1) (clojure.string/split-lines (slurp "resources/numbers.txt"))))
 
-(defn cartesian [report times]
-  (let [list ((partial repeat times) report)]
-    (apply combo/cartesian-product list)))
+(defn combo-data [numbers]
+ (let [s (apply + numbers)
+       p (apply * numbers)]
+   {:sum s
+    :product p}))
 
-(defn cartesian-data [numbers]
-  (let [s (apply + numbers)
-        p (apply * numbers)]
-    {:sum s
-     :product p}))
-
-(defn cartesian-data-map [data times]
-  (map #(cartesian-data %1) (cartesian data times)))
+(defn combo-data-map [data times]
+  (map #(combo-data %1) (combo/combinations data times)))
 
 (defn distinct-by [f coll]
   (let [groups (group-by f coll)]
     (map #(first (groups %)) (distinct (map f coll)))))
 
 (defn results [data checksum times]
-  (let [r (distinct (filter #(= checksum (:sum %1)) (cartesian-data-map data times)))]
+  (let [r (distinct (filter #(= checksum (:sum %1)) (combo-data-map data times)))]
     (distinct-by :sum r)))
 
 (def part1 (:product (into {} (results assignment-data 2020 2))))
 
 (def part2 (:product (into {} (results assignment-data 2020 3))))
-
 
 (defn -main
   "I don't do a whole lot ... yet."
