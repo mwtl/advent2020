5934c2bfd9124bab8673db40d119f6c3c7d63d92
Michael Witlox <michael.witlox@greenhousegroup.com>
2020-12-17 22:32:44 +0100

    Day6 pt2

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -4,7 +4,8 @@
             [aoc2020.day3 :as d3]
             [aoc2020.day4 :as d4]
             [aoc2020.day5 :as d5]
-            [aoc2020.day6 :as d6]))
+            [aoc2020.day6 :as d6]
+            [clojure.math.combinatorics :as combo]))
 
 ;(def d1-solution-part1 (d1/solution d1/input 2020 2))
 ;(def d1-solution-part2 (d1/solution d1/input 2020 3))
@@ -19,8 +20,10 @@
 ;(def d4-solution-part2 (d4/p2-solution d4/input))
 
 ;(def d5-solution-part1 (d5/p1-solution d5/input))
-(def d5-solution-part2 d5/p2-solution)
+;(def d5-solution-part2 d5/p2-solution)
 
+;(def d6-solution-part1 d6/p1-solution)
+;(def d6-solution-part2 (d6/p2-solution d6/input))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/aoc2020/day6.clj
+++ src/aoc2020/day6.clj
@@ -1,12 +1,39 @@
 (ns aoc2020.day6
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as cset]))
 
 
 (def input-test (let [res (slurp "resources/day6-test.txt")]
-  (map #(str/replace % #"\n" "") (str/split res #"\n\n"))))
+  (str/split res #"\n\n")))
 
 (def input (let [res (slurp "resources/day6.txt")]
-  (map #(str/replace % #"\n" "") (str/split res #"\n\n"))))
+  (str/split res #"\n\n")))
 
-(def p1-solution [data]
-  (reduce + (map #(count (distinct %)) data)))
+(def questions
+  (let [group-strings (map #(str/replace % #"\n" "") input)]
+    (map #(distinct %) group-strings)))
+
+(def p1-solution
+  (reduce + (map #(count %) questions)))
+
+
+(defn question-groups [data]
+  (map #(str/split-lines %) data))
+
+(defn question-splits [question-group]
+  (map (fn [qg]
+    (map (fn [qr] (str/split qr #"")) qg)) question-group))
+
+(defn count-anwsers
+  ([remaining-anwsers]
+    (let [current (distinct (flatten (take 1 remaining-anwsers)))
+          remaining (next remaining-anwsers)]
+          (if (nil? remaining) (count current) (count-anwsers remaining current))))
+  ([remaining-anwsers prev-anwsers]
+    (let [current (distinct (flatten (take 1 remaining-anwsers)))
+          intersect (cset/intersection (set prev-anwsers) (set current))
+          remaining (next remaining-anwsers)]
+          (if (nil? remaining) (count intersect) (count-anwsers remaining intersect)))))
+
+(defn p2-solution [data]
+  (reduce + (map #(count-anwsers %) (question-splits (question-groups data)))))
